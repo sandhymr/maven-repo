06ee12f037f86d9948861a9a7a26b95bb646844c
sandhymr <mrsandhya19@gmail.com>
2020-12-21 15:09:23 +0530

    added prod method

--- Calculator/src/main/java/com/lti/Calc.java
+++ Calculator/src/main/java/com/lti/Calc.java
@@ -8,11 +8,16 @@ public class Calc {
 	public static int subs(int num1,int num2) {
 		return num1-num2;
 	}
+	public static int prod(int num1,int num2) {
+		return num1-num2;
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Sum : "+add(30,40));
 		System.out.println("Difference : "+subs(90,80));
+		System.out.println("Product : "+prod(90,80));
+
 
 	}
 
