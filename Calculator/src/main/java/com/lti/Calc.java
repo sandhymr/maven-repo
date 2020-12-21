baeb019a0eee1edf4ce4a2ebc9f3e25af137f9f2
sandhymr <mrsandhya19@gmail.com>
2020-12-21 12:43:43 +0530

    created Calc class with add method

---
+++ Calculator/src/main/java/com/lti/Calc.java
@@ -0,0 +1,15 @@
+package com.lti;
+
+public class Calc {
+	
+	public static int add(int num1,int num2) {
+		return num1+num2;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(add(30,40));
+
+	}
+
+}
